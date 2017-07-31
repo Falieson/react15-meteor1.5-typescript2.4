6517c6a1f5882c7da474c0270d757c8d1b9c1be7
Florian Mettetal <florian.mettetal@flex.com>
2017-07-31 16:07:38 -0700

    React 15.6 'Hello World'

---
+++ client/index.js
@@ -0,0 +1,25 @@
+// Meteor & React
+import { Meteor } from 'meteor/meteor';
+import React  from 'react';
+import { render } from 'react-dom'
+
+// Client Configs: Routes
+// import {renderRoutes} from '../imports/startup/client/index.js';
+
+// Redux
+// import { Provider } from 'react-redux';
+// import Store from '../imports/ui/store/store.js';
+
+// Material-UI
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import AppRoot from './Root'
+
+const Demo = (
+  <div>Hello World</div>
+)
+
+Meteor.startup(()=> {
+  // injectTapEventPlugin(); // Required for Material IconMenu (v.0.15.0)
+  render(AppRoot(undefined,undefined,Demo), document.getElementById('root'));
+});
